e99082c6540fda5ccd24c7e8cf9bc0c88618c144
SantiagoBlanco96 <63128092+SantiagoBlanco96@users.noreply.github.com>
2024-02-14 14:31:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    restrict: 'E',
+    scope: {
+      member: '='
+    },
+    templateUrl: 'app/members/skillsMember.html'
+  };
+}
+  
